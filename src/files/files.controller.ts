41ee6bedda883ec8f02cdbe8bfd54a3cc64bfd89
Sergio Barreras <sergiobg.isc@gmail.com>
2024-02-04 16:09:23 -0700

    retorno de secure url

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,25 +1,50 @@
 import {
   Controller,
+  Get,
+  Param,
   ParseFilePipe,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('product')
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/uploads',
+        filename: fileNamer,
+      }),
     }),
   )
   uploadFile(@UploadedFile(ParseFilePipe) file: Express.Multer.File) {
-    return file.originalname;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/uploads/${file.filename}`;
+
+    return secureUrl;
+  }
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
   }
 }
