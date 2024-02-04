759267198d3b2a03dcbc7a64f3c13238fb4f46d5
Sergio Barreras <sergiobg.isc@gmail.com>
2024-02-03 21:39:21 -0700

    seed ejecutada

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,5 +13,15 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises = [];
+
+    products.forEach((product) => {
+      insertPromises.push(this.productService.create(product));
+    });
+
+    await Promise.all(insertPromises);
   }
 }
