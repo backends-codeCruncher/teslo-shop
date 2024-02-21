4f917753d0b90f20bf0549f8532791e15dec3964
Sergio Barreras <sergiobg.isc@gmail.com>
2024-02-21 12:14:36 -0700

    enlazar socket con usuario

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,11 +1,16 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayload } from '../auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
@@ -13,10 +18,23 @@ export class MessagesWsGateway
 {
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  handleConnection(client: Socket) {
-    this.messagesWsService.registerClient(client);
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+
+    let payload: JWTPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
 
     this.wss.emit(
       'clients-updated',
@@ -31,5 +49,28 @@ export class MessagesWsGateway
       'clients-updated',
       this.messagesWsService.getConnectedClients(),
     );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    //messages-from-server
+
+    //! Emite solo al cliente que manda el mensaje.
+    // client.emit('messages-from-server', {
+    //   fullName: 'Soy yo!',
+    //   message: payload.message || 'no message!!',
+    // });
+
+    //! Emitir a todos menos al cliente inicial
+    // client.broadcast.emit('messages-from-server', {
+    //   fullName: 'Soy yo!',
+    //   message: payload.message || 'no message!!',
+    // });
+
+    //! Emite a todos los clientes conectados
+    this.wss.emit('messages-from-server', {
+      fullName: 'Soy yo!',
+      message: payload.message || 'no message!!',
+    });
   }
 }
