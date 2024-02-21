81f5a5447e203dd19404179225ebce75b003fcb5
Sergio Barreras <sergiobg.isc@gmail.com>
2024-02-21 09:32:04 -0700

    clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,15 +1,35 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() wss: Server;
 
+  constructor(private readonly messagesWsService: MessagesWsService) {}
 
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client);
 
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
 
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id);
 
-
-    
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 }
