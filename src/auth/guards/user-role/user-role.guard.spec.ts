1e7ae5117bc844217eeb1682dca607131a67e214
Sergio Barreras <sergiobg.isc@gmail.com>
2025-05-30 03:00:28 -0700

    Test - Guard: Excepciones

--- src/auth/guards/user-role/user-role.guard.spec.ts
+++ src/auth/guards/user-role/user-role.guard.spec.ts
@@ -1,6 +1,10 @@
 import { Reflector } from '@nestjs/core';
 import { UserRoleGuard } from './user-role.guard';
-import { ExecutionContext } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 
 describe('UserRole Guard', () => {
   let guard: UserRoleGuard;
@@ -24,10 +28,49 @@ describe('UserRole Guard', () => {
     expect(guard.canActivate(mockContext)).toBe(true);
   });
 
-  
   it('should return true if not roles are required', () => {
     jest.spyOn(reflector, 'get').mockReturnValue([]);
 
     expect(guard.canActivate(mockContext)).toBe(true);
+  });
+
+  it('should throw BadRequestException if user is not found', () => {
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+    jest.spyOn(mockContext.switchToHttp(), 'getRequest').mockReturnValue({});
+
+    expect(() => guard.canActivate(mockContext)).toThrow(BadRequestException);
+    expect(() => guard.canActivate(mockContext)).toThrow('User not found');
+  });
+
+  it('should return true if user has a valid role', () => {
+    jest.spyOn(reflector, 'get').mockReturnValue(['admin']);
+
+    jest.spyOn(mockContext.switchToHttp(), 'getRequest').mockReturnValue({
+      user: {
+        roles: ['admin'],
+      },
+    });
+
+    expect(guard.canActivate(mockContext)).toBe(true);
+  });
+
+  it('should throw forbidden user lacks a required role', () => {
+    const user = {
+      roles: ['user'],
+      fullName: 'Sergio Barreras',
+    };
+
+    const validRoles = ['admin'];
+
+    jest.spyOn(reflector, 'get').mockReturnValue(validRoles);
+
+    jest.spyOn(mockContext.switchToHttp(), 'getRequest').mockReturnValue({
+      user,
+    });
+
+    expect(() => guard.canActivate(mockContext)).toThrow(ForbiddenException);
+    expect(() => guard.canActivate(mockContext)).toThrow(
+      `User ${user.fullName} need a valid role: [${validRoles}]`,
+    );
   });
 });
