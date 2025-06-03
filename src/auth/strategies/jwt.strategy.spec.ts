ef6a9484e9c6b57ad232a936311a9577e185170d
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-02 17:17:00 -0700

    Test - Funcionamiento de la estrategia

--- src/auth/strategies/jwt.strategy.spec.ts
+++ src/auth/strategies/jwt.strategy.spec.ts
@@ -4,6 +4,9 @@ import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { JWTPayload } from '../interfaces';
+import { UnauthorizedException } from '@nestjs/common';
+import { mock } from 'node:test';
 
 describe('JwtStrategy', () => {
   let strategy: JwtStrategy;
@@ -33,9 +36,48 @@ describe('JwtStrategy', () => {
     }).compile();
 
     strategy = module.get<JwtStrategy>(JwtStrategy);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(strategy).toBeDefined();
+  });
+
+  it('should validate and return user if user exists and is active', async () => {
+    const payload: JWTPayload = { id: '123' };
+    const mockUser = { id: '123', isActive: true } as User;
+
+    jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(mockUser);
+
+    const result = await strategy.validate(payload);
+
+    expect(result).toEqual(mockUser);
+    expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: '123' });
+  });
+
+  it('should throw Unauthorized Exception if user does not exist', async () => {
+    const payload: JWTPayload = { id: '123' };
+
+    jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(null);
+
+    await expect(strategy.validate(payload)).rejects.toThrow(
+      UnauthorizedException,
+    );
+    await expect(strategy.validate(payload)).rejects.toThrow('Token not valid');
+  });
+
+  it('should throw Unauthorized Exception if user is not active', async () => {
+    const payload: JWTPayload = { id: '123' };
+    const mockUser = { id: '123', isActive: false } as User;
+
+    jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(mockUser);
+
+    await expect(strategy.validate(payload)).rejects.toThrow(
+      UnauthorizedException,
+    );
+
+    await expect(strategy.validate(payload)).rejects.toThrow(
+      'User is not active. Talk with and admin',
+    );
   });
 });
