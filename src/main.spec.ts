be4c4ac0456871958d8f38509a3cfd88227c42f0
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-02 23:55:23 -0700

    Pruebas de configuración en el Main

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { bootstrap } from './main';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 jest.mock('@nestjs/core', () => ({
   NestFactory: {
@@ -46,6 +47,8 @@ describe('Main.ts', () => {
     enableCors: jest.Mock;
   };
 
+  let mockLogger: { log: jest.Mock };
+
   beforeEach(() => {
     mockApp = {
       useGlobalPipes: jest.fn(),
@@ -54,12 +57,46 @@ describe('Main.ts', () => {
       enableCors: jest.fn(),
     };
 
+    mockLogger = {
+      log: jest.fn(),
+    };
+
     (NestFactory.create as jest.Mock).mockResolvedValue(mockApp);
+    (Logger as unknown as jest.Mock).mockReturnValue(mockLogger);
   });
 
   it('should create app', async () => {
     await bootstrap();
 
     expect(NestFactory.create).toHaveBeenCalledWith(AppModule);
+    expect(mockLogger.log).toHaveBeenCalledWith(`App running on port 3000`);
+  });
+
+  it('should listen to env port', async () => {
+    process.env.PORT = '4200';
+
+    await bootstrap();
+
+    expect(mockApp.listen).toHaveBeenCalledWith(process.env.PORT);
+  });
+
+  it('should set global prefix', async () => {
+    await bootstrap();
+
+    expect(mockApp.setGlobalPrefix).toHaveBeenCalledWith('api');
+  });
+
+  it('should use global pipes', async () => {
+    await bootstrap();
+
+    expect(mockApp.useGlobalPipes).toHaveBeenCalledWith(
+      expect.objectContaining({
+        errorHttpStatusCode: 400,
+        validatorOptions: expect.objectContaining({
+          forbidNonWhitelisted: true,
+          whitelist: true,
+        }),
+      }),
+    );
   });
 });
