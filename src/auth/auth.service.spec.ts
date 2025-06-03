906d728f6b559a6f4a3050dff8df5f834cfe5658
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-02 22:17:47 -0700

    Test - Crear usuario

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,10 +4,16 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto';
+
+import { bcryptAdapter } from '../config/bcrypt.adapter';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let authService: AuthService;
   let userRepository: Repository<User>;
+
+  const mockTokenValue = 'mock-jwt-token';
 
   beforeEach(async () => {
     const mockUserRepository = {
@@ -17,7 +23,7 @@ describe('AuthService', () => {
     };
 
     const mockJwtService = {
-      sign: jest.fn().mockReturnValue('mock-jwt-token'),
+      sign: jest.fn().mockReturnValue(mockTokenValue),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -40,5 +46,52 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(authService).toBeDefined();
+  });
+
+  it('should create a user and return it with token', async () => {
+    const dto: CreateUserDto = {
+      email: 'test@google.com',
+      fullName: 'Test User',
+      password: 'Ax1Bz2',
+    };
+
+    const user = {
+      email: dto.email,
+      fullName: dto.fullName,
+      id: '1',
+      isActive: true,
+      roles: ['user'],
+    } as User;
+
+    jest.spyOn(userRepository, 'create').mockReturnValue(user);
+    jest.spyOn(bcryptAdapter, 'hash').mockReturnValue('AeomvpASDAefjAdfsn');
+
+    const response = await authService.create(dto);
+
+    expect(bcryptAdapter.hash).toHaveBeenCalledWith(dto.password);
+    expect(response).toEqual({
+      user: {
+        email: user.email,
+        fullName: user.fullName,
+        id: user.id,
+        isActive: user.isActive,
+        roles: user.roles,
+      },
+      token: mockTokenValue,
+    });
+  });
+
+  it('should throw an error if email already exists', async () => {
+    const dto: CreateUserDto = {
+      email: 'test@google.com',
+      fullName: 'Test User',
+      password: 'Ax1Bz2',
+    };
+
+    jest
+      .spyOn(userRepository, 'save')
+      .mockRejectedValue({ code: '23505', detail: 'Email already exist' });
+
+    await expect(authService.create(dto)).rejects.toThrow(BadRequestException);
   });
 });
