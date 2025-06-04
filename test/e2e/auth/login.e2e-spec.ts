8cb07acf755683910be8ac170c3e155e83f7f75c
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-04 16:39:40 -0700

    E2E - Auth - Login

---
+++ test/e2e/auth/login.e2e-spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+
+describe('Auth - Login (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/auth/login (POST) - should throw 400 if no body', async () => {
+    const response = await request(app.getHttpServer()).post('/auth/login');
+
+    const errorMessages = [
+      'email must be an email',
+      'email must be a string',
+      'The password must have a Uppercase, lowercase letter and a number',
+      'password must be shorter than or equal to 50 characters',
+      'password must be longer than or equal to 6 characters',
+      'password must be a string',
+    ];
+
+    expect(response.statusCode).toBe(400);
+
+    errorMessages.forEach((message) => {
+      expect(response.body.message).toContain(message);
+    });
+  });
+});
